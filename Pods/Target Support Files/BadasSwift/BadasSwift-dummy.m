ac9a277c0501637f7d1e9c8d4a6b98b9ef9c7ede
Morgan Berger <morgan.berger71@gmail.com>
2019-01-15 17:02:18 +0100

    CLEAN GITIGNORE FILES FROM GIT

--- Pods/Target Support Files/BadasSwift/BadasSwift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_BadasSwift : NSObject
-@end
-@implementation PodsDummy_BadasSwift
-@end
--- Pods/Target Support Files/Pods-SuperContacts/Pods-SuperContacts-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SuperContacts : NSObject
-@end
-@implementation PodsDummy_Pods_SuperContacts
-@end
